9a992e4c91a42c492765b84110e475786c570889
GH Palash <golamhabibpalash@gmail.com>
2020-07-22 10:41:00 +0600

    Class Added for ECG Analyzer

---
+++ ECG Analyzer/ECG_All_Classes/RDataFind.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECG_All_Classes
+{
+    public class RDataFind
+    {
+        public bool RData(int data)
+        {
+            int taoData = Convert.ToInt32((0.4) * data);
+            if (data)
+            {
+
+            }
+            return true;
+        }
+    }
+}
